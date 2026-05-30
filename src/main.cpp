3160fe228024a2fa0189e381fd9d599fd55c3907
kravchSof <kravch2202@gmail.com>
2026-05-30 18:40:04 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include "alg.h"
+
 #include <iostream>
 #include <string>
-
-void makeTree(BST<std::string>& tree, const char* filename);
-void printFreq(BST<std::string>& tree);
 
 int main() {
     BST<std::string> tree;
