2a64403a4a2cd360fc7a5bc02a4ef40aa8302f99
kravchSof <kravch2202@gmail.com>
2026-05-30 18:38:40 +0300

    Create alg.h

---
+++ include/alg.h
@@ -0,0 +1,10 @@
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_ALG_H_
+#define INCLUDE_ALG_H_
+
+#include "bst.h"
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
+
+#endif  // INCLUDE_ALG_H_
